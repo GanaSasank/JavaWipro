66a999e77032498b1937bcd64004e4e0346306c8
GanaSasank <1nh19me032.ganasasankreddy@gmail.com>
2025-02-22 14:44:24 +0530

    Upoaded from Laptop using Git Bash

--- fp.java
+++ fp.java
@@ -5,15 +5,16 @@
 public class fp {
 
     public static void main(String[] args) {
-        int a=0;
+        int a = 0;
         System.out.println(a);
         a++;
         System.out.println(a);
         ++a;
         System.out.println(a);
-        a=a+1;
+        a = a + 1;
         System.out.println(a);
-        a=a-1;
+        a = a - 1;
         System.out.println(a);
+        System.out.println("Made changed in Laptop");
     }
 }
