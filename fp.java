041c0a6264b0014bfce74fbe2b8c2aab2e46cfc1
GanaSasank <1nh19me032.ganasasankreddy@gmail.com>
2025-02-22 14:08:57 +0530

    tracked unetracked files

---
+++ fp.java
@@ -0,0 +1,19 @@
+// byte code stores a data upto -128 to 127 bits
+/**
+ * fp
+ */
+public class fp {
+
+    public static void main(String[] args) {
+        int a=0;
+        System.out.println(a);
+        a++;
+        System.out.println(a);
+        ++a;
+        System.out.println(a);
+        a=a+1;
+        System.out.println(a);
+        a=a-1;
+        System.out.println(a);
+    }
+}
